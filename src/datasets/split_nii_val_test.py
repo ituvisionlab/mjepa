3287406315330031c28afc71539b4143f6aabd23
Unal <unalg01@bigpurple-ln4.cm.cluster>
2025-01-01 12:43:42 -0500

    Classifier metrics (auroc, f1, ...) added. Node size 2. create_splits are changed to prevent subject leakage

--- src/datasets/split_nii_val_test.py
+++ src/datasets/split_nii_val_test.py
@@ -2,18 +2,30 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 # Load the CSV file
-file_path = '/gpfs/home/unalg01/jepa/src/datasets/oasis_val_split.csv'  # Replace with your CSV file path
+file_path = '/gpfs/home/unalg01/jepa/src/datasets/adni_new_val_split.csv' #ppmi_val_split.csv 
 data = pd.read_csv(file_path)
 
 # Specify the split ratio
-val_ratio = 0.8  # 80% 
-test_ratio = 0.2    # 20% test
+train_ratio = 0.8  # 80% training
+val_ratio = 0.2    # 20% validation
 
-# Split the data
-train_data, val_data = train_test_split(data, test_size=test_ratio, random_state=42)
+# Ensure there is a 'subject_id' column in the data
+group_col = 'subject_id'  # Correct column name for subject IDs
+if group_col not in data.columns:
+    raise ValueError(f"The column '{group_col}' does not exist in the provided CSV file.")
+
+# Get unique subject IDs
+unique_subjects = data[group_col].unique()
+
+# Split the subjects into train and validation sets
+train_subjects, val_subjects = train_test_split(unique_subjects, test_size=val_ratio, random_state=42)
+
+# Filter the original data based on the split subjects
+train_data = data[data[group_col].isin(train_subjects)]
+val_data = data[data[group_col].isin(val_subjects)]
 
 # Save the splits to new CSV files
-train_data.to_csv('/gpfs/home/unalg01/jepa/src/datasets/oasis_valtrain_split.csv', index=False)
-val_data.to_csv('/gpfs/home/unalg01/jepa/src/datasets/oasis_valtest_split.csv', index=False)
+train_data.to_csv('/gpfs/home/unalg01/jepa/src/datasets/adni_new_valtrain_split.csv', index=False)
+val_data.to_csv('/gpfs/home/unalg01/jepa/src/datasets/adni_new_valtest_split.csv', index=False)
 
 print("Data has been split and saved to 'valtrain_split.csv' and 'valtest_split.csv'.")
